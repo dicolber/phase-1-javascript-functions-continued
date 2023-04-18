e3736869351cceaa0c6fa0fb3a1e5e39b0497c83
dicola berry <dicolber@amazon.com>
2023-04-17 20:33:01 -0400

    continuing functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function  saturdayFun (activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('have a party')
+
+const mondayWork = function mondayWork(work="go to the office") {
+   return `This Monday, I will ${work}.`
+}
+mondayWork('This Monday, I will work from home.')
+
+function wrapAdjective(star = "*") {
+  return function(adjective = "special") {
+    return `You are ${star}${adjective}${star}!`;
+  }
+}
